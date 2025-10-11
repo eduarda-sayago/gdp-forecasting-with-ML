a6939a11b20568a0bfa67d0bdc77af9cc1f9923e
eduarda-sayago <eduarda.sayago@gmail.com>
2025-10-11 00:35:10 -0300

    Dummies, SARIMA fix, LASSO and SARIMA re-run

--- 00_main_ibc.R
+++ 00_main_ibc.R
@@ -72,9 +72,36 @@ datasetm <- as.data.frame(datasetm$df)
 type_dfw <- datasetw$type_df
 datasetw <- as.data.frame(datasetw$df)
 
+# Adding dummies in datasetm
+
+datasetm$date <- as.Date(datasetm$date, origin = "1970-01-01")
+
+dummies <- data.frame(matrix(ncol = 0, nrow = nrow(datasetm)))
+dummies$month <- lubridate::month(datasetm$date)
+
+datasetm$M2 <- ifelse(dummies$month == 2, 1, 0)
+datasetm$M3 <- ifelse(dummies$month == 3, 1, 0)
+datasetm$M4 <- ifelse(dummies$month == 4, 1, 0)
+datasetm$M5 <- ifelse(dummies$month == 5, 1, 0)
+datasetm$M6 <- ifelse(dummies$month == 6, 1, 0)
+datasetm$M7 <- ifelse(dummies$month == 7, 1, 0)
+datasetm$M8 <- ifelse(dummies$month == 8, 1, 0)
+datasetm$M9 <- ifelse(dummies$month == 9, 1, 0)
+datasetm$M10 <- ifelse(dummies$month == 10, 1, 0)
+datasetm$M11 <- ifelse(dummies$month == 11, 1, 0)
+datasetm$M12 <- ifelse(dummies$month == 12, 1, 0)
+datasetm$d_pandemic <- ifelse(datasetm$date >= as.Date("2020-01-01") &
+                               datasetm$date <= as.Date("2020-10-01"), 1, 0)
+#datasetm$d_rsflood <- ifelse(datasetm$date == as.Date("2024-06-01"), 1, 0)
+datasetm$d_shift <- ifelse(datasetm$date < as.Date("2013-01-01"), 
+                           seq_len(sum(datasetm$date < as.Date("2013-01-01"))),0)
+
+# Adding dummies in datasetw
+
+
 # Storing and removing Date column
-date = datasetm$date
-dfdate = as.Date(date, origin = "1970-01-01")
+dfdate = datasetm$date
+# dfdate = as.Date(date, origin = "1970-01-01")
 
 datasetm$date <- NULL
 datasetm[] <- lapply(datasetm, as.numeric)
@@ -106,25 +133,25 @@ mean_model <- call_models(datasetw, 'Mean - IBC-w', get_mean, "ibc_rs")
 message("SARIMA")
 
 benchmarkp <- call_models(dataset, 'SARIMA - PIB_RS', get_sarima, "pib_rs")
-# h=1 RMSE: 0.07011117; MAE: 0.04844195; MAPE:     
-# h=4 RMSE: 0.12220252; MAE: 0.10155142; MAPE:  
+# h=1 RMSE: 0.07011117; MAE: 0.04844195; MAPE: 99.58476     
+# h=4 RMSE: 0.07980111; MAE: 0.05570668; MAPE: 127.10611  
 
-benchmark <- call_models(datasetm, 'SARIMA - IBC-m', get_sarima, "ibc_rs")
-# h=1 RMSE: 0.06629558; MAE: 0.04880438; MAPE:
-# h=12 RMSE: 0.10242225; MAE: 0.07750083; MAPE: 
+benchmark <- call_models1(datasetm, 'SARIMA - IBC-m', get_sarima, "ibc_rs")
+# h=1 RMSE: 0.06629558; MAE: 0.04880438; MAPE: 1.048171 
+# h=12 RMSE: 0.09837429; MAE: 0.07007968; MAPE: 1.501778  
 
 #=====
 message("LASSO")
 
 lasso_modelp <- call_models(dataset, 'LASSO - PIB_RS', get_lasso, "pib_rs")
-# h=1 RMSE: 0.05920526  ; MAE: 0.04084625; MAPE:  
-# h=4 RMSE: 0.05988952  ; MAE: 0.04352576; MAPE: 
+# h=1 RMSE: 0.05920526  ; MAE: 0.04084625; MAPE: 119.5896 
+# h=4 RMSE: 0.05988952  ; MAE: 0.04352576; MAPE: 105.9388 
 
-lasso_model <- call_models(datasetm, 'LASSO - IBC-m', get_lasso, "ibc_rs")
-# h=1 RMSE: 0.04012251 ; MAE:  0.03249728; MAPE: 
-# h=12 RMSE: 0.06383114 ; MAE: 0.04716723; MAPE: 
+lasso_model <- call_models1(datasetm, 'LASSO - IBC-m', get_lasso, "ibc_rs")
+# h=1 RMSE: 0.03649598; MAE: 0.02839302; MAPE: 0.6150090 
+# h=12 RMSE: 0.05989764; MAE: 0.04312109; MAPE: 0.9387088 
 
-lasso_wmodel <- call_models(datasetw, 'LASSO - IBC-w', get_lasso, "ibc_rs")
+lasso_wmodel <- call_models1(datasetw, 'LASSO - IBC-w', get_lasso, "ibc_rs")
 # h=1 RMSE: 0.03860373 ; MAE:  0.02976507; MAPE:
 # h=12 RMSE: 0.07399347 ; MAE: 0.05377384; MAPE:
 
@@ -134,10 +161,10 @@ message("Elastic Net")
 enet_modelp <- call_models(dataset, 'Elastic Net', get_elasticnet, "pib_rs")
 # h=1 RMSE: 0.05790124 ; MAE: 0.04586020; MAPE:    
 # h=4 RMSE: 0.12473872 ; MAE: 0.09587616; MAPE:
-enet_model <- call_models(datasetm, 'Elastic Net', get_elasticnet, "ibc_rs")
+enet_model <- call_models1(datasetm, 'Elastic Net', get_elasticnet, "ibc_rs")
 # h=1 RMSE: ; MAE:    ; MAPE:
 # h=4 RMSE: ; MAE:    ; MAPE:
-enet_wmodel <- call_models(datasetw, 'Elastic Net', get_elasticnet, "ibc_rs")
+enet_wmodel <- call_models1(datasetw, 'Elastic Net', get_elasticnet, "ibc_rs")
 # h=1 RMSE: ; MAE:    ; MAPE:
 # h=4 RMSE: ; MAE:    ; MAPE:
 
@@ -145,19 +172,19 @@ enet_wmodel <- call_models(datasetw, 'Elastic Net', get_elasticnet, "ibc_rs")
 #=====
 message("Random Forest")
 
-rf_model <- call_models(datasetm, 'Random Forest', get_rf, "ibc_rs")
+rf_model <- call_models1(datasetm, 'Random Forest', get_rf, "ibc_rs")
 # h=1 RMSE: ; MAE:     
 # h=4 RMSE: ; MAE:  
-rf_wmodel <- call_models(datasetw, 'Random Forest', get_rf, "ibc_rs")
+rf_wmodel <- call_models1(datasetw, 'Random Forest', get_rf, "ibc_rs")
 # h=1 RMSE: ; MAE:     
 # h=4 RMSE: ; MAE:  
 
 message("Boosting")
 
-boost_model <- call_models(datasetm, 'Boosting', get_boosting, "ibc_rs")
+boost_model <- call_models1(datasetm, 'Boosting', get_boosting, "ibc_rs")
 # h=1 RMSE: ; MAE:      
 # h=4 RMSE: ; MAE:   
-boost_wmodel <- call_models(datasetw, 'Boosting', get_boosting, "ibc_rs")
+boost_wmodel <- call_models1(datasetw, 'Boosting', get_boosting, "ibc_rs")
 # h=1 RMSE: ; MAE:      
 # h=4 RMSE: ; MAE:  
 
